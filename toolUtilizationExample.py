e50f499a41f262e92981c777790ee277cea33014
Giovanni Henggar Setyantoro <47100048+JavaBlangkon@users.noreply.github.com>
2019-02-05 01:35:43 +0700

    Add files via upload

---
+++ toolUtilizationExample.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import twoBodyTool
+import numpy as np
+from scipy.integrate import odeint
+from twoBodyTool import F, sv2coe
+
+# Defining initial value of position and velocity vector
+x0 = -6045
+y0 = -3490
+z0 = 2500
+vx0 = -3.457
+vy0 = 6.618
+vz0 = 2.533
+
+# Defining period of the orbit in second and its interval
+t = np.linspace(0,8200,1000)
+# Defining the solution as the result of second ode integral
+solution = odeint(F,[x0,y0,z0,vx0,vy0,vz0],t)
+
+# Defining results as aa empty array for plotting
+results = []
+# Example of loop function of the results data set
+for data in range(0,1000):
+    r_test = np.squeeze(solution[data:data+1,0:3])
+    v_test = np.squeeze(solution[data:data+1,3:6])
+    t1 = np.linspace(0,8200,data+1)
+    results.append(sv2coe(r_test, v_test))
+
+# Final form of results in array
+results = np.array(results)
+
+# Example of how to plot the relationships between orbital elements and the period
+import matplotlib.pyplot as plt
+x1 = results[:,0]
+y1 = t1
+plt.plot(y1,x1, '.')
+plt.axis('equal')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
